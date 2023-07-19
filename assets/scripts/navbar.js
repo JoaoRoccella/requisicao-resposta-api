175017d087eb0eff13f7c51f8b36a83faf12d2d5
Joao Roccella <joao.marcos@sp.senai.br>
2023-07-19 15:44:01 -0300

    Criando o menu de navegação

---
+++ assets/scripts/navbar.js
@@ -0,0 +1,7 @@
+const navbar = document.createElement('nav');
+navbar.className = 'navbar';
+
+const aHome = document.createElement('a');
+aHome
+
+document.querySelector('header').insertBefore(navbar, document.querySelector('h1'));
